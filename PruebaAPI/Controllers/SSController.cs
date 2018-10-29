fed92732994c4263536fce59edb245e9a00bc798
Andres Ignacio Diaz Medina <1520557@utp.edu.pe>
2018-10-29 17:29:16 -0500

    Se realizan las 4 APIs

--- PruebaAPI/Controllers/SSController.cs
+++ PruebaAPI/Controllers/SSController.cs
@@ -12,18 +12,98 @@ namespace PruebaAPI.Controllers
         // GET: SS
         public ActionResult ListadoPatologias()
         {
-
-            patologia[] patologias = { new patologia() { Nombre = "Diarrea" }, new patologia() {Nombre = "Nauseas" } };
-
-            //var js = new JavaScriptSerializer().Serialize(patologias);
-            return Json(patologias,JsonRequestBehavior.AllowGet);
+            using (Entities obj = new Entities())
+            {
+                var pato = obj.patologia;
+                var patologias = pato.ToList();
+                //var js = new JavaScriptSerializer().Serialize(patologias);
+                return Json(patologias, JsonRequestBehavior.AllowGet);
+            }
         }
 
-        public ActionResult ProductoPorPatologia()
+        public ActionResult ProductoPatologia()
+        {
+            var old_id = Request.QueryString["id"];
+            int id = int.Parse(old_id.ToString());
+
+            using (Entities obj = new Entities())
+            {
+
+                var x = from popa in obj.patologia_producto
+                        where popa.idPatologia == id
+                        select popa.idProducto;
+
+                var productos_id = x.ToList();
+
+                var y = obj.producto;
+                var productos_obj = y.ToList();
+                List<producto> productos = new List<producto>();
+
+
+                foreach (producto prod in productos_obj)
+                {
+                    if (productos_id.Contains(prod.idProducto)) {
+                        productos.Add(prod);
+                    }
+                }
+                return Json(productos, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        public ActionResult DetalleProducto()
+        {
+            var old_id = Request.QueryString["id"];
+            //int id = int.Parse(old_id.ToString());
+
+            using (Entities obj = new Entities())
+            {
+
+                var x =  obj.producto.ToList();
+
+                List<producto> productos = new List<producto>();
+
+                foreach (producto prod in x)
+                {
+                    if (prod.idProducto.Equals(old_id)) productos.Add(prod);
+                }
+                
+
+                return Json(productos, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult FavoritoProducto()
         {
             var id = Request.QueryString["id"];
-            return Json(id, JsonRequestBehavior.AllowGet);
+            var old_bool = Request.QueryString["fav"];
+            bool favi = Convert.ToBoolean(old_bool);
+            
+
+            using (Entities obj = new Entities())
+            {
+
+                var x = obj.patologia_producto.ToList();
+
+                foreach (patologia_producto papo in x)
+                {
+                    if (papo.idProducto.Equals(id)) papo.flagfav = favi;
+                }
+
+                obj.SaveChanges();
+
+
+                string vacio = "Subida completada";
+                //producto vacio = new producto();
+             
+                return Json(vacio, JsonRequestBehavior.AllowGet);
+            }
+           
         }
-        
     }
+
+
+
+
 }
